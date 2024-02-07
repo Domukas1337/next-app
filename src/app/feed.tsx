98638ad262d481347bd9c0c9932bab537ab84401
napsev1 <matasnematas@gmail.com>
2024-02-07 19:16:13 +0200

    feed 0.3v

---
+++ src/app/feed.tsx
@@ -0,0 +1,11 @@
+
+export default function Feed() {
+    return (
+        <div className="inline-flex items-center justify-center border-2 w-6/12">
+            <div className="p-2 ">
+                <h1>For you</h1>
+                <h1>Following</h1>
+            </div>
+        </div>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
+import Feed from "./feed";
+
 export default function Home() {
   return (
     <main>
       <div>
-        <h1 className="text-3xl font-bold underline">Hello world!</h1>
+        <Feed/>
       </div>
     </main>
   );
