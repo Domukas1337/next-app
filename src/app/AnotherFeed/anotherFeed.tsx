1fe54a8024ac4b931cb8d36f38a2c75c47f19a4f
Domukas1337 <domukas1337@gmail.com>
2024-02-08 16:07:16 +0200

    search bar commit

--- src/app/AnotherFeed/anotherFeed.tsx
+++ src/app/AnotherFeed/anotherFeed.tsx
@@ -1,10 +1,9 @@
+import Search from "./Search";
+
 export default function AnotherFeed() {
     return (
-        <div className="flex items-center justify-center border-2 w-2/4">
-            <h1>Another</h1>
-            <h1>Another</h1>
-            <h1>Another</h1>
-            <h1>Another</h1>
+        <div className="flex flex-row p-2 justify-center border-2 w-1/2">
+            <Search />
         </div>
     );
 }
