526695040a4882288cf1995cc3c3790fe3c50870
Domukas1337 <domukas1337@gmail.com>
2024-02-10 18:29:51 +0200

    more feed items

--- src/app/AnotherFeed/popularFeedItems.ts
+++ src/app/AnotherFeed/popularFeedItems.ts
@@ -2,8 +2,18 @@ const popularFeedItems = [
     {
         heading: "Huitter bangar!!!",
         description: "Huitter is an amazing website. I love it so much.",
-        postcount: "5", 
-    }
+        postcount: "5324", 
+    },
+    {
+        heading: "Kanye West",
+        description: "Kanye is an amazing website. I love it so much.",
+        postcount: "524", 
+    },
+    {
+        heading: "Trevis SCut",
+        description: "Trevis is an amazing website. I love it so much.",
+        postcount: "5645", 
+    },
 ]
 
 export default popularFeedItems;
